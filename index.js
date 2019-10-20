9cc79d12093595e1be62f2c4e28f2e0c4ae79891
Dhuha Hasan <duha_9@yahoo.com>
2019-10-20 19:38:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let driver={name:'Dhuha',filedStudy:'computer science'};
+let newDriver={};
+
+function updateDriverWithKeyAndValue(driver,key,value){
+  return Object.assign({},driver,{[key]:value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key, value){
+  driver[key] = value;
+
+  return driver;
+ 
+}
+
+function deleteFromDriverByKey(driver,key){
+  
+  newDriver=Object.assign({},driver);
+  delete newDriver[key];
+  return newDriver;
+ 
+}
+
+function destructivelyDeleteFromDriverByKey(driver,key){
+  
+  delete driver[key];
+  return driver;
+ 
+}
