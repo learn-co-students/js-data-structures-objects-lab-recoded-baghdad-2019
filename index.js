775c2d7d69683923f6457a111076e87efad6c9a7
Sura Al-jilawi <suraaljailawi@gmail.com>
2020-01-14 18:18:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+
+// Write your solution in this file!
+let driver={name:'Sura',filedStudy:'computer science'};
+let newDriver={};
+
+function updateDriverWithKeyAndValue(driver,key,value){
+  return Object.assign({},driver,{[key]:value});
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key, value){
+  driver[key] = value;
+
+  return driver;
+
+}
+
+function deleteFromDriverByKey(driver,key){
+
+  newDriver=Object.assign({},driver);
+  delete newDriver[key];
+  return newDriver;
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver,key){
+
+  delete driver[key];
+  return driver;
+
+}
